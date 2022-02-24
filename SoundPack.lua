88e69a1a9dcc5dcd1038b3f5b519820155985ef7
Travis Spomer <16262858+TravisSpomer@users.noreply.github.com>
2022-02-24 08:21:26 -0800

    Initial commit

---
+++ SoundPack.lua
@@ -0,0 +1,206 @@
+﻿-- Hear Kitty official sound packs  by Vger-Azjol-Nerub
+-- www.vgermods.com
+------------------------------------------------------------
+
+if KittyRegisterSoundPack then
+local LocalizedName
+
+------------------------------------------------------------
+LocalizedName = "Symphony (quiet)"
+if (GetLocale() == "frFR") then
+	LocalizedName = "Symphonie (calme)"
+elseif (GetLocale() == "esES" or GetLocale() == "esMX") then
+	LocalizedName = "Sinfonía (tranquilo)"
+end
+KittyRegisterSoundPack("SymphonyQuiet", {
+	LocalizedName = LocalizedName,
+	Credits = KittyUIFrame_AboutHeaderLabel_Text,
+	SoundDelay = 0.35,
+	LongSoundDelay = 1,
+	
+	Combo6StackSound0 = "Interface\\AddOns\\HearKitty_SoundPacks\\SymphonyQuiet\\0.ogg",
+	Combo6StackSound1 = "Interface\\AddOns\\HearKitty_SoundPacks\\SymphonyQuiet\\2.ogg",
+	Combo6StackSound2 = "Interface\\AddOns\\HearKitty_SoundPacks\\SymphonyQuiet\\1.ogg",
+	Combo6StackSound3 = "Interface\\AddOns\\HearKitty_SoundPacks\\SymphonyQuiet\\2.ogg",
+	Combo6StackSound4 = "Interface\\AddOns\\HearKitty_SoundPacks\\SymphonyQuiet\\3.ogg",
+	Combo6StackSound5 = "Interface\\AddOns\\HearKitty_SoundPacks\\SymphonyQuiet\\4.ogg",
+	Combo6StackSound6 = "Interface\\AddOns\\HearKitty_SoundPacks\\SymphonyQuiet\\5.ogg",
+	
+	Combo5StackSound0 = "Interface\\AddOns\\HearKitty_SoundPacks\\SymphonyQuiet\\0.ogg",
+	Combo5StackSound1 = "Interface\\AddOns\\HearKitty_SoundPacks\\SymphonyQuiet\\1.ogg",
+	Combo5StackSound2 = "Interface\\AddOns\\HearKitty_SoundPacks\\SymphonyQuiet\\2.ogg",
+	Combo5StackSound3 = "Interface\\AddOns\\HearKitty_SoundPacks\\SymphonyQuiet\\3.ogg",
+	Combo5StackSound4 = "Interface\\AddOns\\HearKitty_SoundPacks\\SymphonyQuiet\\4.ogg",
+	Combo5StackSound5 = "Interface\\AddOns\\HearKitty_SoundPacks\\SymphonyQuiet\\5.ogg",
+	
+	Combo4StackSound0 = "Interface\\AddOns\\HearKitty_SoundPacks\\SymphonyQuiet\\0.ogg",
+	Combo4StackSound1 = "Interface\\AddOns\\HearKitty_SoundPacks\\SymphonyQuiet\\1.ogg",
+	Combo4StackSound2 = "Interface\\AddOns\\HearKitty_SoundPacks\\SymphonyQuiet\\2.ogg",
+	Combo4StackSound3 = "Interface\\AddOns\\HearKitty_SoundPacks\\SymphonyQuiet\\4.ogg",
+	Combo4StackSound4 = "Interface\\AddOns\\HearKitty_SoundPacks\\SymphonyQuiet\\5.ogg",
+	
+	Combo3StackSound0 = "Interface\\AddOns\\HearKitty_SoundPacks\\SymphonyQuiet\\0.ogg",
+	Combo3StackSound1 = "Interface\\AddOns\\HearKitty_SoundPacks\\SymphonyQuiet\\3.ogg",
+	Combo3StackSound2 = "Interface\\AddOns\\HearKitty_SoundPacks\\SymphonyQuiet\\4.ogg",
+	Combo3StackSound3 = "Interface\\AddOns\\HearKitty_SoundPacks\\SymphonyQuiet\\5.ogg",
+})
+
+------------------------------------------------------------
+LocalizedName = "Symphony (loud)"
+if (GetLocale() == "frFR") then
+	LocalizedName = "Symphonie (fort)"
+elseif (GetLocale() == "esES" or GetLocale() == "esMX") then
+	LocalizedName = "Sinfonía (fuerte)"
+end
+KittyRegisterSoundPack("SymphonyLoud", {
+	LocalizedName = LocalizedName,
+	Credits = KittyUIFrame_AboutHeaderLabel_Text,
+	SoundDelay = 0.35,
+	LongSoundDelay = 1,
+
+	Combo6StackSound0 = "Interface\\AddOns\\HearKitty_SoundPacks\\SymphonyLoud\\0.ogg",
+	Combo6StackSound1 = "Interface\\AddOns\\HearKitty_SoundPacks\\SymphonyLoud\\2.ogg",
+	Combo6StackSound2 = "Interface\\AddOns\\HearKitty_SoundPacks\\SymphonyLoud\\1.ogg",
+	Combo6StackSound3 = "Interface\\AddOns\\HearKitty_SoundPacks\\SymphonyLoud\\2.ogg",
+	Combo6StackSound4 = "Interface\\AddOns\\HearKitty_SoundPacks\\SymphonyLoud\\3.ogg",
+	Combo6StackSound5 = "Interface\\AddOns\\HearKitty_SoundPacks\\SymphonyLoud\\4.ogg",
+	Combo6StackSound6 = "Interface\\AddOns\\HearKitty_SoundPacks\\SymphonyLoud\\5.ogg",
+	
+	Combo5StackSound0 = "Interface\\AddOns\\HearKitty_SoundPacks\\SymphonyLoud\\0.ogg",
+	Combo5StackSound1 = "Interface\\AddOns\\HearKitty_SoundPacks\\SymphonyLoud\\1.ogg",
+	Combo5StackSound2 = "Interface\\AddOns\\HearKitty_SoundPacks\\SymphonyLoud\\2.ogg",
+	Combo5StackSound3 = "Interface\\AddOns\\HearKitty_SoundPacks\\SymphonyLoud\\3.ogg",
+	Combo5StackSound4 = "Interface\\AddOns\\HearKitty_SoundPacks\\SymphonyLoud\\4.ogg",
+	Combo5StackSound5 = "Interface\\AddOns\\HearKitty_SoundPacks\\SymphonyLoud\\5.ogg",
+	
+	Combo4StackSound0 = "Interface\\AddOns\\HearKitty_SoundPacks\\SymphonyLoud\\0.ogg",
+	Combo4StackSound1 = "Interface\\AddOns\\HearKitty_SoundPacks\\SymphonyLoud\\1.ogg",
+	Combo4StackSound2 = "Interface\\AddOns\\HearKitty_SoundPacks\\SymphonyLoud\\2.ogg",
+	Combo4StackSound3 = "Interface\\AddOns\\HearKitty_SoundPacks\\SymphonyLoud\\4.ogg",
+	Combo4StackSound4 = "Interface\\AddOns\\HearKitty_SoundPacks\\SymphonyLoud\\5.ogg",
+	
+	Combo3StackSound0 = "Interface\\AddOns\\HearKitty_SoundPacks\\SymphonyLoud\\0.ogg",
+	Combo3StackSound1 = "Interface\\AddOns\\HearKitty_SoundPacks\\SymphonyLoud\\3.ogg",
+	Combo3StackSound2 = "Interface\\AddOns\\HearKitty_SoundPacks\\SymphonyLoud\\4.ogg",
+	Combo3StackSound3 = "Interface\\AddOns\\HearKitty_SoundPacks\\SymphonyLoud\\5.ogg",
+})
+
+------------------------------------------------------------
+LocalizedName = "Crystal"
+if (GetLocale() == "frFR" or GetLocale() == "esES" or GetLocale() == "esMX") then
+	LocalizedName = "Cristal"
+end
+KittyRegisterSoundPack("Crystal", {
+	LocalizedName = LocalizedName,
+	Credits = KittyUIFrame_AboutHeaderLabel_Text,
+	SoundDelay = 0.35,
+	LongSoundDelay = 1,
+
+	Combo6StackSound0 = "Interface\\AddOns\\HearKitty_SoundPacks\\Crystal\\0.ogg",
+	Combo6StackSound1 = "Interface\\AddOns\\HearKitty_SoundPacks\\Crystal\\2.ogg",
+	Combo6StackSound2 = "Interface\\AddOns\\HearKitty_SoundPacks\\Crystal\\1.ogg",
+	Combo6StackSound3 = "Interface\\AddOns\\HearKitty_SoundPacks\\Crystal\\2.ogg",
+	Combo6StackSound4 = "Interface\\AddOns\\HearKitty_SoundPacks\\Crystal\\1.ogg",
+	Combo6StackSound5 = "Interface\\AddOns\\HearKitty_SoundPacks\\Crystal\\4.ogg",
+	Combo6StackSound6 = "Interface\\AddOns\\HearKitty_SoundPacks\\Crystal\\5.ogg",
+	
+	Combo5StackSound0 = "Interface\\AddOns\\HearKitty_SoundPacks\\Crystal\\0.ogg",
+	Combo5StackSound1 = "Interface\\AddOns\\HearKitty_SoundPacks\\Crystal\\1.ogg",
+	Combo5StackSound2 = "Interface\\AddOns\\HearKitty_SoundPacks\\Crystal\\2.ogg",
+	Combo5StackSound3 = "Interface\\AddOns\\HearKitty_SoundPacks\\Crystal\\1.ogg",
+	Combo5StackSound4 = "Interface\\AddOns\\HearKitty_SoundPacks\\Crystal\\4.ogg",
+	Combo5StackSound5 = "Interface\\AddOns\\HearKitty_SoundPacks\\Crystal\\5.ogg",
+	
+	Combo4StackSound0 = "Interface\\AddOns\\HearKitty_SoundPacks\\Crystal\\0.ogg",
+	Combo4StackSound1 = "Interface\\AddOns\\HearKitty_SoundPacks\\Crystal\\1.ogg",
+	Combo4StackSound2 = "Interface\\AddOns\\HearKitty_SoundPacks\\Crystal\\2.ogg",
+	Combo4StackSound3 = "Interface\\AddOns\\HearKitty_SoundPacks\\Crystal\\4.ogg",
+	Combo4StackSound4 = "Interface\\AddOns\\HearKitty_SoundPacks\\Crystal\\5.ogg",
+	
+	Combo3StackSound0 = "Interface\\AddOns\\HearKitty_SoundPacks\\Crystal\\0.ogg",
+	Combo3StackSound1 = "Interface\\AddOns\\HearKitty_SoundPacks\\Crystal\\1.ogg",
+	Combo3StackSound2 = "Interface\\AddOns\\HearKitty_SoundPacks\\Crystal\\4.ogg",
+	Combo3StackSound3 = "Interface\\AddOns\\HearKitty_SoundPacks\\Crystal\\5.ogg",
+})
+
+------------------------------------------------------------
+LocalizedName = "Crystal (quiet)"
+if (GetLocale() == "frFR") then
+	LocalizedName = "Cristal (calme)"
+elseif (GetLocale() == "esES" or GetLocale() == "esMX") then
+	LocalizedName = "Cristal (tranquilo)"
+end
+KittyRegisterSoundPack("CrystalQuiet", {
+	LocalizedName = LocalizedName,
+	Credits = KittyUIFrame_AboutHeaderLabel_Text,
+	SoundDelay = 0.35,
+	LongSoundDelay = 1,
+
+	Combo6StackSound0 = "Interface\\AddOns\\HearKitty_SoundPacks\\CrystalQuiet\\0.ogg",
+	Combo6StackSound1 = "Interface\\AddOns\\HearKitty_SoundPacks\\CrystalQuiet\\2.ogg",
+	Combo6StackSound2 = "Interface\\AddOns\\HearKitty_SoundPacks\\CrystalQuiet\\1.ogg",
+	Combo6StackSound3 = "Interface\\AddOns\\HearKitty_SoundPacks\\CrystalQuiet\\2.ogg",
+	Combo6StackSound4 = "Interface\\AddOns\\HearKitty_SoundPacks\\CrystalQuiet\\1.ogg",
+	Combo6StackSound5 = "Interface\\AddOns\\HearKitty_SoundPacks\\CrystalQuiet\\4.ogg",
+	Combo6StackSound6 = "Interface\\AddOns\\HearKitty_SoundPacks\\CrystalQuiet\\5.ogg",
+	
+	Combo5StackSound0 = "Interface\\AddOns\\HearKitty_SoundPacks\\CrystalQuiet\\0.ogg",
+	Combo5StackSound1 = "Interface\\AddOns\\HearKitty_SoundPacks\\CrystalQuiet\\1.ogg",
+	Combo5StackSound2 = "Interface\\AddOns\\HearKitty_SoundPacks\\CrystalQuiet\\2.ogg",
+	Combo5StackSound3 = "Interface\\AddOns\\HearKitty_SoundPacks\\CrystalQuiet\\1.ogg",
+	Combo5StackSound4 = "Interface\\AddOns\\HearKitty_SoundPacks\\CrystalQuiet\\4.ogg",
+	Combo5StackSound5 = "Interface\\AddOns\\HearKitty_SoundPacks\\CrystalQuiet\\5.ogg",
+	
+	Combo4StackSound0 = "Interface\\AddOns\\HearKitty_SoundPacks\\CrystalQuiet\\0.ogg",
+	Combo4StackSound1 = "Interface\\AddOns\\HearKitty_SoundPacks\\CrystalQuiet\\1.ogg",
+	Combo4StackSound2 = "Interface\\AddOns\\HearKitty_SoundPacks\\CrystalQuiet\\2.ogg",
+	Combo4StackSound3 = "Interface\\AddOns\\HearKitty_SoundPacks\\CrystalQuiet\\4.ogg",
+	Combo4StackSound4 = "Interface\\AddOns\\HearKitty_SoundPacks\\CrystalQuiet\\5.ogg",
+	
+	Combo3StackSound0 = "Interface\\AddOns\\HearKitty_SoundPacks\\CrystalQuiet\\0.ogg",
+	Combo3StackSound1 = "Interface\\AddOns\\HearKitty_SoundPacks\\CrystalQuiet\\1.ogg",
+	Combo3StackSound2 = "Interface\\AddOns\\HearKitty_SoundPacks\\CrystalQuiet\\4.ogg",
+	Combo3StackSound3 = "Interface\\AddOns\\HearKitty_SoundPacks\\CrystalQuiet\\5.ogg",
+})
+
+------------------------------------------------------------
+LocalizedName = "Powerup"
+if (GetLocale() == "frFR") then
+	LocalizedName = "PowerUp"
+elseif (GetLocale() == "esES" or GetLocale() == "esMX") then
+	LocalizedName = "Encendido"
+end
+KittyRegisterSoundPack("Powerup", {
+	LocalizedName = LocalizedName,
+	Credits = KittyUIFrame_AboutHeaderLabel_Text,
+	SoundDelay = 0.35,
+	LongSoundDelay = 1,
+
+	Combo6StackSound0 = "Interface\\AddOns\\HearKitty_SoundPacks\\Powerup\\0.ogg",
+	Combo6StackSound1 = "Interface\\AddOns\\HearKitty_SoundPacks\\Powerup\\2.ogg",
+	Combo6StackSound2 = "Interface\\AddOns\\HearKitty_SoundPacks\\Powerup\\1.ogg",
+	Combo6StackSound3 = "Interface\\AddOns\\HearKitty_SoundPacks\\Powerup\\2.ogg",
+	Combo6StackSound4 = "Interface\\AddOns\\HearKitty_SoundPacks\\Powerup\\1.ogg",
+	Combo6StackSound5 = "Interface\\AddOns\\HearKitty_SoundPacks\\Powerup\\4.ogg",
+	Combo6StackSound6 = "Interface\\AddOns\\HearKitty_SoundPacks\\Powerup\\5.ogg",
+	
+	Combo5StackSound0 = "Interface\\AddOns\\HearKitty_SoundPacks\\Powerup\\0.ogg",
+	Combo5StackSound1 = "Interface\\AddOns\\HearKitty_SoundPacks\\Powerup\\1.ogg",
+	Combo5StackSound2 = "Interface\\AddOns\\HearKitty_SoundPacks\\Powerup\\2.ogg",
+	Combo5StackSound3 = "Interface\\AddOns\\HearKitty_SoundPacks\\Powerup\\1.ogg",
+	Combo5StackSound4 = "Interface\\AddOns\\HearKitty_SoundPacks\\Powerup\\4.ogg",
+	Combo5StackSound5 = "Interface\\AddOns\\HearKitty_SoundPacks\\Powerup\\5.ogg",
+	
+	Combo4StackSound0 = "Interface\\AddOns\\HearKitty_SoundPacks\\Powerup\\0.ogg",
+	Combo4StackSound1 = "Interface\\AddOns\\HearKitty_SoundPacks\\Powerup\\1.ogg",
+	Combo4StackSound2 = "Interface\\AddOns\\HearKitty_SoundPacks\\Powerup\\2.ogg",
+	Combo4StackSound3 = "Interface\\AddOns\\HearKitty_SoundPacks\\Powerup\\4.ogg",
+	Combo4StackSound4 = "Interface\\AddOns\\HearKitty_SoundPacks\\Powerup\\5.ogg",
+	
+	Combo3StackSound0 = "Interface\\AddOns\\HearKitty_SoundPacks\\Powerup\\0.ogg",
+	Combo3StackSound1 = "Interface\\AddOns\\HearKitty_SoundPacks\\Powerup\\1.ogg",
+	Combo3StackSound2 = "Interface\\AddOns\\HearKitty_SoundPacks\\Powerup\\4.ogg",
+	Combo3StackSound3 = "Interface\\AddOns\\HearKitty_SoundPacks\\Powerup\\5.ogg",
+})
+
+end
